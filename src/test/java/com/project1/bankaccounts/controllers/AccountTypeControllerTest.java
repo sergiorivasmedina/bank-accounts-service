573e188358bdab4ceaf72a43e7a3ac7ebb8a7364
Sergio Rivas <sergio.rivas@pucp.pe>
2020-07-14 11:40:56 -0500

    :white_check_mark: account type tests

--- src/test/java/com/project1/bankaccounts/controllers/AccountTypeControllerTest.java
+++ src/test/java/com/project1/bankaccounts/controllers/AccountTypeControllerTest.java
@@ -1,8 +1,10 @@
 package com.project1.bankaccounts.controllers;
 
+import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.times;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.project1.bankaccounts.models.AccountType;
@@ -14,11 +16,15 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = AccountTypeController.class)
@@ -51,5 +57,43 @@ public class AccountTypeControllerTest {
             .expectBodyList(AccountType.class);
 
             Mockito.verify(accountTypeRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void newAccountType() {
+        AccountType accountType = new AccountType("1", "ahorro");
+
+        Mockito
+            .when(accountTypeRepository.save(accountType))
+            .thenReturn(Mono.just(accountType));
+        
+        webclient.post()
+            .uri("/account/type/new")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromValue(accountType))
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(AccountType.class);
+
+        Mockito.verify(accountTypeRepository, times(1)).save(refEq(accountType));
+    }
+
+    @Test
+    public void deleteAccountType() {
+        AccountType accountType = new AccountType("1", "ahorro");
+
+        Mockito
+            .when(accountTypeRepository.findById("1"))
+            .thenReturn(Mono.just(accountType));
+
+        Mono<Void> voidReturn  = Mono.empty();
+        Mockito
+            .when(accountTypeRepository.delete(accountType))
+            .thenReturn(voidReturn);
+
+	    webclient.delete()
+                .uri("/account/type/{typeId}", accountType.getIdAccountType())
+                .exchange()
+                .expectStatus().isOk();
     }
 }
