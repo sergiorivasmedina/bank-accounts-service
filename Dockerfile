13a7e4209366939435c65098e0859da6afd7b12b
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-18 18:45:09 -0500

    :sparkles: logback added

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM maven:latest
+FROM openjdk:8-alpine
 MAINTAINER srivasme@everis.com
 
-COPY ./target/ ./app
-EXPOSE 8080
+COPY ./target ./app
+EXPOSE 8081
 WORKDIR ./app/
 ENTRYPOINT ["java", "-jar", "bank-accounts-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/bootcamp/bankaccounts/App.java
+++ src/main/java/com/bootcamp/bankaccounts/App.java
@@ -2,6 +2,7 @@ package com.bootcamp.bankaccounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,6 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2WebFlux
 public class App {
 
