a3d950fbc828e56ede5b21a9fd7f9b4034f5f80d
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-18 23:38:40 -0500

    :sparkles: numberTransactionsRemainder added

--- src/main/java/com/bootcamp/bankaccounts/models/BankAccount.java
+++ src/main/java/com/bootcamp/bankaccounts/models/BankAccount.java
@@ -5,6 +5,13 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "bankAccount")
 public class BankAccount {
     @Id
@@ -15,86 +22,5 @@ public class BankAccount {
     private List<String> idCustomer; //Spanish: Titulares de la cuenta -> Customers
     private List<String> authorizedSignatories; //Spanish: Firmantes autorizados -> Customers
     private List<String> transactions;
-
-    public BankAccount() {
-    }
-
-    public BankAccount(String idBankAccount, double availableBalance, String accountType, String currency, List<String> idCustomer, List<String> authorizedSignatories, List<String> transactions) {
-        this.idBankAccount = idBankAccount;
-        this.availableBalance = availableBalance;
-        this.accountType = accountType;
-        this.currency = currency;
-        this.idCustomer = idCustomer;
-        this.authorizedSignatories = authorizedSignatories;
-        this.transactions = transactions;
-    }
-
-    public String getIdBankAccount() {
-        return this.idBankAccount;
-    }
-
-    public void setIdBankAccount(String idBankAccount) {
-        this.idBankAccount = idBankAccount;
-    }
-
-    public double getAvailableBalance() {
-        return this.availableBalance;
-    }
-
-    public void setAvailableBalance(double availableBalance) {
-        this.availableBalance = availableBalance;
-    }
-
-    public String getAccountType() {
-        return this.accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public String getCurrency() {
-        return this.currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public List<String> getIdCustomer() {
-        return this.idCustomer;
-    }
-
-    public void setIdCustomer(List<String> idCustomer) {
-        this.idCustomer = idCustomer;
-    }
-
-    public List<String> getAuthorizedSignatories() {
-        return this.authorizedSignatories;
-    }
-
-    public void setAuthorizedSignatories(List<String> authorizedSignatories) {
-        this.authorizedSignatories = authorizedSignatories;
-    }
-
-    public List<String> getTransactions() {
-        return this.transactions;
-    }
-
-    public void setTransactions(List<String> transactions) {
-        this.transactions = transactions;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " idBankAccount='" + getIdBankAccount() + "'" +
-            ", availableBalance='" + getAvailableBalance() + "'" +
-            ", accountType='" + getAccountType() + "'" +
-            ", currency='" + getCurrency() + "'" +
-            ", idCustomer='" + getIdCustomer() + "'" +
-            ", authorizedSignatories='" + getAuthorizedSignatories() + "'" +
-            ", transactions='" + getTransactions() + "'" +
-            "}";
-    }
+    private int numberTransactionsRemainder;
 }
