8e4df70a1b4b367ad6db8bee3cf414d17c5d204e
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-21 11:59:33 -0500

    :wrench: move swagger configurations

--- src/main/java/com/bootcamp/bankaccounts/App.java
+++ src/main/java/com/bootcamp/bankaccounts/App.java
@@ -3,39 +3,12 @@ package com.bootcamp.bankaccounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableSwagger2WebFlux
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
-	private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Reactive Documentation - Project 1")
-                .description("Reactive API Documentation - Sergio Rivas Medina")
-                .version("1.0.0")
-                .build();
-        }
-        @Bean
-        public Docket docket() {
-                return new Docket(DocumentationType.SWAGGER_2)
-                        .apiInfo(this.apiInfo())
-                        .enable(true)
-                        .select()
-                        .paths(PathSelectors.any())
-                        .build();
-        }
-
 }
