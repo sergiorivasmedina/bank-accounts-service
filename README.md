f8e6f94bea34aa7c70b95d9344fe98789325be08
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-08-03 18:57:08 -0500

    :memo: architecture image updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project2 - bank Accounts
+# Project4 updated - bank Accounts
 Java Spring Boot Backend connected to mongodb.
 
 ### Command for run Dockerfile and start container
@@ -7,6 +7,11 @@ cd /bank-accounts
 docker build -t "accounts"
 
 docker run --restart always --name accounts -8080:9000 -d accounts:latest
+
+### Using Docker Compose
+cd /bank-accounts
+
+docker-compose up -d
 
 ### Sonarqube
 docker run -d --name sonarqube -p 8090:9000 sonarqube
--- src/main/java/com/bootcamp/bankaccounts/controllers/BankAccountController.java
+++ src/main/java/com/bootcamp/bankaccounts/controllers/BankAccountController.java
@@ -17,8 +17,6 @@ import com.bootcamp.bankaccounts.services.CommissionService;
 import com.bootcamp.bankaccounts.services.TransactionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
